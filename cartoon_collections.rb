86ef79ddb1eda52d8aea38f51544ebc819344d5f
Chase Hilbert <chase@heyspacetime.com>
2019-02-05 02:54:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,6 @@ def long_planeteer_calls(planeteer)
 end
 
 def find_the_cheese(cheese)
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types = ["cheddar"]
   cheese.include?(cheese_types)
 end
