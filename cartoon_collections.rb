19d62092262b5361b8e881f2e527feed46dc62a4
Chase Hilbert <chase@heyspacetime.com>
2019-02-05 02:25:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,9 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(veggies)
-  veggies.size
+  veggies.each do |vegetable|
+    vegetable.upcase
+
 end
 
 def long_planeteer_calls# code an argument here
